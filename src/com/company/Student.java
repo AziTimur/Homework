6fc7baae81ee9010b95e2150c44e40d7705adfcf
AziTimur <azik00kgz@gmail.com>
2021-05-04 04:20:23 +0600

    wwe

---
+++ src/com/company/Student.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public final class Student extends National {
+    private int cours;
+    private Gender gender;
+
+    public Student(String name, int age,String national,String instruments,int cours,Gender gender) {
+        super(name, age,national,instruments);
+        //super.getNational();
+        this.cours=cours;
+        this.gender=gender;
+    }
+
+    public int getCours() {
+        return cours;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    @Override
+    public String getInfo() {
+      return   super.getInfo()+ " "+getNational()+" "+getInstruments()+" "+getCours()+"й курс "+getGender();
+    }
+}
